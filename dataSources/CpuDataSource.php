908b73dd4185a31fbb15cf1f763829c1567c09c0
AnatolyRugalev <anatoly.rugalev@gmail.com>
2014-09-01 10:31:44 +0600

    CPU DataSource fixed

--- dataSources/CpuDataSource.php
+++ dataSources/CpuDataSource.php
@@ -10,12 +10,12 @@ class CpuDataSource extends PerformanceDataSource
 		$graphs = array(
 			'cpu' => array(
 				'series' => array(
-					'time' => array('type' => 'spline', 'data' => array()),
+					'cpu' => array('type' => 'spline', 'data' => array()),
 				),
 			),
 		);
 		foreach ($data as $row) {
-			$graphs['cpu']['series']['time']['data'][] = array(strtotime($row['date']) * 1000, (int)$row['time'] / 1000);
+			$graphs['cpu']['series']['cpu']['data'][] = array(strtotime($row['date']) * 1000, (int)$row['cpu'] / 1000);
 		}
 		return $graphs;
 	}
