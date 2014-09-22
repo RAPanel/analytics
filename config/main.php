45207ecd5b77fe53a115a88d094d9b0933050424
AnatolyRugalev <anatoly.rugalev@gmail.com>
2014-09-22 10:33:52 +0600

    urlRules moved to its owners

--- config/main.php
+++ config/main.php
@@ -5,5 +5,12 @@ return array(
 			'class' => 'analytics.components.AnalyticsComponent',
 			'enabled' => true,
 		),
+		'urlManager' => array(
+			'rules' => array(
+				'<m_:(analytics)>' => array('<m_>//'),
+				'<m_:(analytics)>/<c_>' => array('<m_>/<c_>/index'),
+				'<m_:(analytics)>/<c_>/<a_>' => array('<m_>/<c_>/<a_>'),
+			),
+		),
 	),
 );
