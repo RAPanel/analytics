ec9b5531e6dad73f2f011aabc4b8efa05f14b6a4
Anatoly Rugalev <anatoly.rugalev@gmail.com>
2014-10-16 01:19:40 +0600

    Added moduleMenu items to config

--- config/web.php
+++ config/web.php
@@ -14,5 +14,14 @@ return CMap::mergeArray(require(__DIR__ . '/main.php'), array(
 				'AnalyticsComponent::onApplicationEnd'
 			),
 		),
+		'moduleMapper' => array(
+			'menuExtra' => array(
+				'Статистика' => array(
+					'Просмотры' => array('analytics/graph', 'dataSources' => 'hits,visits,visitors', 'graphZoom' => '7d'),
+					'Производительность' => array('analytics/graph', 'dataSources' => 'ram,cpu,execTime', 'graphZoom' => '7d'),
+					'Всё' => array('analytics/graph', 'dataSources' => 'hits,visits,visitors,ram,cpu,execTime', 'graphZoom' => '7d'),
+				),
+			),
+		),
 	),
 ));
